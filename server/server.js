a5e7ff29b720dfd28d9c1c6e7c8a9ea512e45a0e
tyler broadway <tbroadway96@gmail.com>
2020-05-31 20:28:58 -0500

    Created schema & database

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const projectsRouter = require('./projectsRouter');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+
+module.exports = server;
